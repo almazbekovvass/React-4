ee2c4cf23c5e07d9fddaa64737344c3893d3d673
almazbekovvass <almazbekkayperi>
2024-03-19 21:13:26 +0600

    dz4

---
+++ dz4/src/App.js
@@ -0,0 +1,17 @@
+import AgePage from "./agePage/AgePage";
+import TurnPage from "./turnPage/TurnPage";
+import InputPage from "./inputPage/InputPage";
+import FormPage from "./formPage/FormPage";
+
+function App() {
+  return (
+    <div style={{textAlign:'center'}}>
+        <AgePage/>
+        <TurnPage/>
+        <InputPage/>
+        <FormPage/>
+    </div>
+  );
+}
+
+export default App;
